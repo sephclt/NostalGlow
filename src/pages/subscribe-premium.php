4c442d705e559e768805d74ecd4e8c776a14a344
sephclt <josephajero22@gmail.com>
2024-03-20 10:06:25 +0800

    added subscription sessions

--- src/pages/subscribe-premium.php
+++ src/pages/subscribe-premium.php
@@ -6,5 +6,7 @@ $conn = mysqli_connect('db', 'root', '', 'my_online_store');
 $sql = "UPDATE `profiles` SET `subscription` = 7 WHERE `username` = '$username'";
 mysqli_query($conn, $sql);
 
+$_SESSION['subscription'] = 7;
+
 header("Location: user-dashboard.php");
 exit();
