2bab88d1e84700e7edf7e31fbbd5f59b7714538a
sephclt <josephajero22@gmail.com>
2024-03-20 09:53:18 +0800

    added subscription scripts

---
+++ src/pages/subscribe-basic.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$username = $_SESSION['username'];
+
+$conn = mysqli_connect('db', 'root', '', 'my_online_store');
+$sql = "UPDATE `profiles` SET `subscription` = 5 WHERE `username` = '$username'";
+mysqli_query($conn, $sql);
+
+header("Location: user-dashboard.php");
+exit();
