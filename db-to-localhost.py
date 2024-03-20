c1d6437f24fc92d2888f2a71901743f74c6a386c
sephclt <josephajero22@gmail.com>
2024-03-21 00:08:46 +0800

    added a script to change db to localhost

---
+++ db-to-localhost.py
@@ -0,0 +1,31 @@
+import os
+import re
+import sys
+
+dir_path = sys.argv[1]
+
+
+def replace_db_with_localhost(file_path):
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+
+        with open(file_path, "w") as file:
+            for line in lines:
+                line = re.sub(r"\bdb\b", "localhost", line)
+                file.write(line)
+
+    except Exception as e:
+        print(f"Error processing file: {file_path}")
+        print(e)
+
+
+def process_php_files(directory):
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            if file_name.endswith(".php"):
+                file_path = os.path.join(root, file_name)
+                replace_db_with_localhost(file_path)
+
+
+process_php_files(dir_path)
