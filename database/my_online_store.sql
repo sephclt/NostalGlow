b38bf49a40fff43293d70f7a343f596192e039e4
joseph <joseph.clutario@ciit.edu.ph>
2024-03-21 11:32:44 +0800

    database update

--- database/my_online_store.sql
+++ database/my_online_store.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: db:3306
--- Generation Time: Mar 20, 2024 at 02:58 PM
--- Server version: 8.3.0
--- PHP Version: 8.2.8
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2024 at 04:32 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `log_table` (
-  `log_id` int NOT NULL,
+  `log_id` int(11) NOT NULL,
   `action` varchar(30) NOT NULL,
-  `timestamp` timestamp NOT NULL
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -48,10 +48,18 @@ INSERT INTO `log_table` (`log_id`, `action`, `timestamp`) VALUES
 --
 
 CREATE TABLE `messages` (
-  `message_id` int NOT NULL,
+  `message_id` int(11) NOT NULL,
   `sender_email` varchar(50) NOT NULL,
   `sender_message` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `sender_email`, `sender_message`) VALUES
+(1, 'emailko@gmail.com', 'Mema sabi'),
+(2, 'emailko@gmail.com', 'Mema sabi');
 
 -- --------------------------------------------------------
 
@@ -60,14 +68,14 @@ CREATE TABLE `messages` (
 --
 
 CREATE TABLE `products` (
-  `item_id` int NOT NULL,
-  `item_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(30) NOT NULL,
   `price` decimal(10,1) DEFAULT NULL,
-  `capsule` int DEFAULT NULL,
-  `themes` enum('Basic','Advanced','Extended') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `encryption` enum('Server End','End-to-End') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `integration` enum('Basic','Advanced','Extended') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `newFeatures` enum('Priority') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+  `capsule` int(11) DEFAULT NULL,
+  `themes` enum('Basic','Advanced','Extended') DEFAULT NULL,
+  `encryption` enum('Server End','End-to-End') DEFAULT NULL,
+  `integration` enum('Basic','Advanced','Extended') DEFAULT NULL,
+  `newFeatures` enum('Priority') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -86,15 +94,15 @@ INSERT INTO `products` (`item_id`, `item_name`, `price`, `capsule`, `themes`, `e
 --
 
 CREATE TABLE `profiles` (
-  `user_id` int NOT NULL,
-  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `first_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `contact` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `subscription` int DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contact` varchar(12) NOT NULL,
+  `subscription` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -124,8 +132,8 @@ DELIMITER ;
 --
 
 CREATE TABLE `user_memories` (
-  `memory_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `memory_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `memory_link` varchar(100) NOT NULL,
   `memory_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -137,7 +145,8 @@ CREATE TABLE `user_memories` (
 INSERT INTO `user_memories` (`memory_id`, `user_id`, `memory_link`, `memory_name`) VALUES
 (1, 1, 'https://www.youtube.com/watch?v=RGeWKnr9hZY', 'favorite memory'),
 (3, 1, 'https://www.youtube.com/watch?v=q9zKYh8sY_E', 'singing guy'),
-(4, 3, 'https://github.com/sephclt/NostalGlow', 'My Website');
+(4, 3, 'https://github.com/sephclt/NostalGlow', 'My Website'),
+(5, 3, 'https://www.youtube.com/watch?v=sVTy_wmn5SU&pp=ygUMbmV3amVhbnMgb21n', 'malupet na song');
 
 --
 -- Indexes for dumped tables
@@ -182,31 +191,31 @@ ALTER TABLE `user_memories`
 -- AUTO_INCREMENT for table `log_table`
 --
 ALTER TABLE `log_table`
-  MODIFY `log_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `message_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `item_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_memories`
 --
 ALTER TABLE `user_memories`
-  MODIFY `memory_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `memory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
